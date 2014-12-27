95e11ac88c0caf0d7884b6ae606ed275be02880d
Esteban Pintos <epintos7@gmail.com>
2014-12-27 17:49:18 -0300

    Added average per interview

--- js/models/interview.js
+++ js/models/interview.js
@@ -2,5 +2,15 @@ App.Interview = DS.Model.extend({
   createdAt: DS.attr('date'),
   interviewExercises: DS.hasMany('interviewExercise', { async: true }),
   interviewers: DS.hasMany('interviewer', { async: true }),
-  interviewee: DS.belongsTo('interviewee', { async: true })
+  interviewee: DS.belongsTo('interviewee', { async: true }),
+  totalMark: function(){
+    var marks = [];
+    marks.pushObjects(this.get('interviewExercises').map(function(interviewExercise) {
+        return interviewExercise.get('mark') || 0;
+    }))
+    if(marks.length){
+      return marks.reduce(function(pv, cv) { return pv + cv; }, 0) / marks.length;
+    }
+    return 0;
+  }.property('interviewExercises.@each.mark')
 });
